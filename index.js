136b73f622ddcbff929e9ad96ff9c4e7c70c8c7e
Lizzie <lchan217@gmail.com>
2019-05-28 10:27:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  const input = document;
+  let i = 0;
 
+  input.addEventListener('keydown', function(e){
+    const key = parseInt(e.detail || e.which);
+    if (key === code[i]) {
+     i++;
+
+       if (i === code.length) {
+         alert("Yay you got it!");
+         i = 0;
+       }
+     } else {
+       i = 0;
+     }
+  })
 }
