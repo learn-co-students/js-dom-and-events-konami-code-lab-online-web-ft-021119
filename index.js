e81176daa26c9b6390eae81ce76ccf22c002f763
Lizzie <lchan217@gmail.com>
2019-05-28 10:29:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  const input = document;
+  const input = document.body;
   let i = 0;
 
   input.addEventListener('keydown', function(e){
