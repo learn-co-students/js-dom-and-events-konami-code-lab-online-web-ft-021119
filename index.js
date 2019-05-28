ab6a5546d9e693d6488b87063b00ec0714b99090
github username <github email address>
2019-05-28 16:59:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let index = 0
+
 function init() {
+  document.body.addEventListener('keydown', e => {
+    const key = parseInt(e.detail || e.which)
+    if(key === code[index]){
+    index++
+    if(index === code.length){
+      alert('Hurray')
+      index = 0
+    }
+  } else {
+    index = 0
+  }
+
+  })
   // Write your JavaScript code inside the init() function
 
 }
