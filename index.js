e9e4cbfe6786771acf8b43f76e8051f84b1f2918
bbennett7 <bryn.bennett@gmail.com>
2019-05-30 15:03:24 -0700

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
 
+const body = document.querySelector('body');
+
+let input = [];
+
+function init() {
+  body.addEventListener('keydown', function(e) {
+
+
+    if (e.keyCode === code[input.length]) {
+      input.push(e.keyCode);
+      if (input.length === code.length) {
+        alert("Hurray!");
+      }
+    } else {
+        input = [];
+    }
+  });
 }
